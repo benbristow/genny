e793a4bf2c3fd7631647a212d4ece5d77c0ef065
Ben Bristow <ben@bristow.me>
2025-12-15 13:15:55 +0000

    feat: set executable name for CLI application

--- Genny/Program.cs
+++ Genny/Program.cs
@@ -4,6 +4,7 @@ using Genny.Commands;
 return await new CliApplicationBuilder()
     .AddCommand<BuildSiteCommand>()
     .SetTitle("Genny")
+    .SetExecutableName("Genny")
     .SetDescription("A static site generator")
     .Build()
     .RunAsync(args);
